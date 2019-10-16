532d69710f8088b1ff083205a033d4e882a36b0e
Chad Chaney <cchane4@gmail.com>
2019-10-16 13:54:01 -0500

    latest syntax using request promise and es6

--- placeholder.js
+++ placeholder.js
@@ -1,11 +1,9 @@
-const request = require("request"); 
+const rp = require("request-promise"); 
 
-request('https://jsonplaceholder.typicode.com/users/1', (error, response, body) => {
-  //eval(require('locus'))
-  if(!error && response.statusCode == 200){ 
-      const parsed_data = JSON.parse(body); 
-      // the dollar sign and brackets are a part of template literal syntax 
-      // it allows dynamic the string we created inside the parentheses to 
-      console.log(`${parsed_data.name} lives in ${parsed_data.address.city}`); 
-  }
-});
+rp('https://jsonplaceholder.typicode.com/users/1').then 
+((body) => {
+const parsed_data = JSON.parse(body); 
+ console.log(`${parsed_data.name} lives in ${parsed_data.address.city}`); 
+}).catch((err) => { 
+  console.log("Error!", err);
+  })
